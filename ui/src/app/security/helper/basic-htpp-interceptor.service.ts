56378434723aa1a17d44c778d1f4485c946cd2db
Kristina <miss.krisi@abv.bg>
2019-12-26 19:13:37 +0200

    works

--- ui/src/app/security/helper/basic-htpp-interceptor.service.ts
+++ ui/src/app/security/helper/basic-htpp-interceptor.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
 import { AuthenticationService } from './authentication.service';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasicAuthHtppInterceptorService implements HttpInterceptor {
 
-  constructor() { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    request = request.clone({headers: request.headers.set('Authorization', sessionStorage.getItem('token'))});
 
-    if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: sessionStorage.getItem('token')
-        }
-      });
-    }
-
-    return next.handle(req);
-
+    return next.handle(request);
   }
 }
