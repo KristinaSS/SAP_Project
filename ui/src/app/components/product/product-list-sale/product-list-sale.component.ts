4ebd67a178feb5d5e26ed99fb12c2f2e8774b97f
kristina.stoyanova <kristina.stoyanova@musala.com>
2020-01-09 15:15:40 +0200

    commiting ui

---
+++ ui/src/app/components/product/product-list-sale/product-list-sale.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '@app/services/product.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-product-list-sale',
+  templateUrl: './product-list-sale.component.html',
+  styleUrls: ['./product-list-sale.component.css']
+})
+export class ProductListSaleComponent implements OnInit {
+  public productList;
+
+  /*todo title*/
+
+  constructor(private productService: ProductService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProductListByCampaign('null').subscribe(
+      data => {
+        this.productList = data;
+      },
+      error => console.error(error),
+      () => console.log('Products Loaded')
+    );
+  }
+
+  seeProduct(product) {
+    let url: string;
+    if (this.lsTestAuthentication()) {
+      url = '/product-em/' + product.productId;
+      this.router.navigate([url]);
+    } else {
+      url = '/product/' + product.productId;
+      console.log('url: ' + url);
+      this.router.navigate([url]);
+    }
+  }
+
+  lsTestAuthentication() {
+    let test = 'accountType';
+    try {
+      if (sessionStorage.getItem(test) === 'employee') {
+        return true;
+      }
+      if (sessionStorage.getItem(test) === 'admin') {
+        return true;
+      }
+      if (sessionStorage.getItem(test) === null) {
+        return false;
+      }
+      console.log('true' + sessionStorage.getItem('accountType'));
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
+
+}
